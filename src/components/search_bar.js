7baa88e42c6d6ae3155277fc26744b03eab16a1a
Cleone Jacob <therealcleonejacob@gmail.com>
2017-09-10 03:04:48 -0400

    A few additions. Added a Background image of an early 80's TV set and a placeholder for the search bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -11,6 +11,7 @@ class SearchBar extends Component {
     return (
       <div className="search-bar">
         <input
+           placeholder="Search For Your Favorite Videos Here!"
           value={this.state.term}
           onChange={event => this.onInputChange(event.target.value)} />
       </div>
